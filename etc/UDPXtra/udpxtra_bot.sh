6dc867eaa36d7d8ed27de8d04143f9a7291d2828
mr•vybes <xquame@yahoo.com>
2023-07-18 01:03:34 +0000

    •bot updated #02 @voltssh•

--- etc/UDPXtra/udpxtra_bot.sh
+++ etc/UDPXtra/udpxtra_bot.sh
@@ -3,17 +3,8 @@
 # UDP Xtra Manager Telegram Bot
 # Date: Jul 17, 2023.
 
-# Check if the TOKEN and CHAT_ID environment variables are already set
-if [[ -z $TELEGRAM_BOT_TOKEN || -z $TELEGRAM_CHAT_ID ]]; then
-    echo "Get ChatID here: https://t.me/useridinfobot"
-    echo ""
-    read -p "Enter Telegram bot token: " TOKEN
-    read -p "Enter your ChatID: " CHAT_ID
-
-    # Store the TOKEN and CHAT_ID in environment variables
-    export TELEGRAM_BOT_TOKEN=$TOKEN
-    export TELEGRAM_CHAT_ID=$CHAT_ID
-fi
+TELEGRAM_BOT_TOKEN="6008613985:AAE7BK45KKm7rS_u8agzAUrhD9Du3P74TPI"
+TELEGRAM_CHAT_ID="1559919183"
 
 MAIN_SCRIPT_PATH="/usr/bin/udpx"
 
@@ -23,63 +14,137 @@ get_vps_info() {
 }
 
 start() {
-    keyboard='[["User Management"],["VPS Info"]]'
-    sticker_path="/etc/UDPXtra/srkr.tgs"
+    keyboard='{
+        "inline_keyboard": [
+            [
+                {
+                    "text": "User Management",
+                    "callback_data": "User Management"
+                }
+            ],
+            [
+                {
+                    "text": "VPS Info",
+                    "callback_data": "VPS Info"
+                }
+            ]
+        ]
+    }'
 
-    sticker_message=$(base64 -w 0 "$sticker_path")
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendSticker" -d "chat_id=$TELEGRAM_CHAT_ID" -d "sticker=$sticker_message" > /dev/null
-
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=Welcome to UDP Xtra Manager Bot!" -d "reply_markup={\"keyboard\":$keyboard,\"resize_keyboard\":true}" > /dev/null
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+        -d "chat_id=$TELEGRAM_CHAT_ID" \
+        -d "text=😊 Welcome to UDP Xtra Manager Bot!" \
+        -d "reply_markup=$keyboard" > /dev/null
 }
 
 user_management_menu() {
-    keyboard='[["List Users"],["Add User"],["Delete User"],["Back"]]'
-    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=User Management Menu" -d "reply_markup={\"keyboard\":$keyboard,\"resize_keyboard\":true}" > /dev/null
+    keyboard='{
+        "inline_keyboard": [
+            [
+                {
+                    "text": "List Users",
+                    "callback_data": "List Users"
+                }
+            ],
+            [
+                {
+                    "text": "Add User",
+                    "callback_data": "Add User"
+                },
+                {
+                    "text": "Delete User",
+                    "callback_data": "Delete User"
+                }
+            ],
+            [
+                {
+                    "text": "Back",
+                    "callback_data": "Back"
+                }
+            ]
+        ]
+    }'
+
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+        -d "chat_id=$TELEGRAM_CHAT_ID" \
+        -d "text=User Management Menu" \
+        -d "reply_markup=$keyboard" > /dev/null
 }
 
 button_handler() {
     callback_data=$1
 
-    if [[ $callback_data == "User Management" ]]; then
-        user_management_menu
-    elif [[ $callback_data == "List Users" ]]; then
-        output=$(bash "$MAIN_SCRIPT_PATH" print_users)
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$output" > /dev/null
-    elif [[ $callback_data == "Add User" ]]; then
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
-    elif [[ $callback_data == "Delete User" ]]; then
-        output=$(bash "$MAIN_SCRIPT_PATH" print_users)
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$output" > /dev/null
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=Please enter the username to delete" > /dev/null
-    elif [[ $callback_data == "VPS Info" ]]; then
-        vps_info=$(get_vps_info)
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$vps_info" > /dev/null
-    fi
+    case $callback_data in
+        "User Management")
+            user_management_menu
+            ;;
+        "List Users")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            ;;
+        "Add User")
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
+            ;;
+        "Delete User")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username to delete" > /dev/null
+            ;;
+        "VPS Info")
+            vps_info=$(get_vps_info)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$vps_info" > /dev/null
+            ;;
+    esac
 }
 
 message_handler() {
     text=$1
 
-    if [[ $text == "/start" ]]; then
-        start
-    elif [[ $text == "User Management" ]]; then
-        user_management_menu
-    elif [[ $text == "List Users" ]]; then
-        output=$(bash "$MAIN_SCRIPT_PATH" print_users)
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$output" > /dev/null
-    elif [[ $text == "Add User" ]]; then
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
-    elif [[ $text == "Delete User" ]]; then
-        output=$(bash "$MAIN_SCRIPT_PATH" print_users)
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=$output" > /dev/null
-        curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d "chat_id=$TELEGRAM_CHAT_ID" -d "text=Please enter the username to delete" > /dev/null
-    elif [[ $text == "Back" ]]; then
-        start
-    fi
+    case $text in
+        "/start")
+            start
+            ;;
+        "User Management")
+            user_management_menu
+            ;;
+        "List Users")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            ;;
+        "Add User")
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
+            ;;
+        "Delete User")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username to delete" > /dev/null
+            ;;
+        "Back")
+            start
+            ;;
+    esac
 }
 
-# Start the bot in the background using nohup
-nohup bash -c 'while true; do
+# Start the bot in the background
+while true; do
     start
     while read -r update; do
         callback_data=$(echo "$update" | jq -r ".callback_query.data")
@@ -91,7 +156,167 @@ nohup bash -c 'while true; do
         fi
     done
     sleep 1
-done' > /dev/null 2>&1 &
+done
 
 echo "Bot is running..."
-echo "Press enter to go menu!"
+#!/bin/bash
+
+# UDP Xtra Manager Telegram Bot
+# Date: Jul 17, 2023.
+
+TELEGRAM_BOT_TOKEN="6008613985:AAE7BK45KKm7rS_u8agzAUrhD9Du3P74TPI"
+TELEGRAM_CHAT_ID="1559919183"
+
+MAIN_SCRIPT_PATH="/usr/bin/udpx"
+
+get_vps_info() {
+    output=$(bash "$MAIN_SCRIPT_PATH" show_vps)
+    echo "$output"
+}
+
+start() {
+    keyboard='{
+        "inline_keyboard": [
+            [
+                {
+                    "text": "User Management",
+                    "callback_data": "User Management"
+                }
+            ],
+            [
+                {
+                    "text": "VPS Info",
+                    "callback_data": "VPS Info"
+                }
+            ]
+        ]
+    }'
+
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+        -d "chat_id=$TELEGRAM_CHAT_ID" \
+        -d "text=😊 Welcome to UDP Xtra Manager Bot!" \
+        -d "reply_markup=$keyboard" > /dev/null
+}
+
+user_management_menu() {
+    keyboard='{
+        "inline_keyboard": [
+            [
+                {
+                    "text": "List Users",
+                    "callback_data": "List Users"
+                }
+            ],
+            [
+                {
+                    "text": "Add User",
+                    "callback_data": "Add User"
+                },
+                {
+                    "text": "Delete User",
+                    "callback_data": "Delete User"
+                }
+            ],
+            [
+                {
+                    "text": "Back",
+                    "callback_data": "Back"
+                }
+            ]
+        ]
+    }'
+
+    curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+        -d "chat_id=$TELEGRAM_CHAT_ID" \
+        -d "text=User Management Menu" \
+        -d "reply_markup=$keyboard" > /dev/null
+}
+
+button_handler() {
+    callback_data=$1
+
+    case $callback_data in
+        "User Management")
+            user_management_menu
+            ;;
+        "List Users")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            ;;
+        "Add User")
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
+            ;;
+        "Delete User")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username to delete" > /dev/null
+            ;;
+        "VPS Info")
+            vps_info=$(get_vps_info)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$vps_info" > /dev/null
+            ;;
+    esac
+}
+
+message_handler() {
+    text=$1
+
+    case $text in
+        "/start")
+            start
+            ;;
+        "User Management")
+            user_management_menu
+            ;;
+        "List Users")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            ;;
+        "Add User")
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username, password, and expiry date in the format: username password expiry_date" > /dev/null
+            ;;
+        "Delete User")
+            output=$(bash "$MAIN_SCRIPT_PATH" print_users)
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=$output" > /dev/null
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                -d "chat_id=$TELEGRAM_CHAT_ID" \
+                -d "text=Please enter the username to delete" > /dev/null
+            ;;
+        "Back")
+            start
+            ;;
+    esac
+}
+
+# Start the bot in the background
+while true; do
+    start
+    while read -r update; do
+        callback_data=$(echo "$update" | jq -r ".callback_query.data")
+        message_text=$(echo "$update" | jq -r ".message.text")
+        if [[ -n $callback_data ]]; then
+            button_handler "$callback_data"
+        elif [[ -n $message_text ]]; then
+            message_handler "$message_text"
+        fi
+    done
+    sleep 1
+done
+
+echo "Bot is running..."
